2ba05bba65821439b4f63de94f75f51b15016244
Jason Pond <pond@mtnmeetings.com>
2025-07-30 11:48:37 -0500

    update to APP

---
+++ my-app/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'    // ← make sure this path is exact
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',        // if you have one at project root
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
