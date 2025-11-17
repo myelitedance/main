47c70ce4431b9310bb5f85375febf1196ba9b139
Jason Pond <jason@myelitedance.com>
2025-11-17 16:03:23 -0600

    Recital Form build x18

--- data/recitalPrices2026.ts
+++ data/recitalPrices2026.ts
@@ -26,9 +26,9 @@ export const recitalPricesByClassId: Record<
   "1088048": { price: 150, allowMultiDiscount: true }, // POINTE
   "1121458": { price: 75, allowMultiDiscount: false }, // TAP ADULT
   "1088211": { price: 75, allowMultiDiscount: false }, // TAP LEVEL 2
-  "1088220": { price: 175, allowMultiDiscount: false }, // TAP/BALLET/JAZZ
-  "1088225": { price: 175, allowMultiDiscount: false }, // TAP/BALLET/JAZZ (Sat
-  "1088224": { price: 150, allowMultiDiscount: false }, // TAP/BALLET/TUMBLING
-  "1088033": { price: 150, allowMultiDiscount: false }, // TAP/BALLET/TUMBLING
-  "1088204": { price: 150, allowMultiDiscount: false }, // TAP/BALLEY/TUMBLING
+  "1088220": { price: 175, allowMultiDiscount: true }, // TAP/BALLET/JAZZ
+  "1088225": { price: 175, allowMultiDiscount: true }, // TAP/BALLET/JAZZ (Sat
+  "1088224": { price: 150, allowMultiDiscount: true }, // TAP/BALLET/TUMBLING
+  "1088033": { price: 150, allowMultiDiscount: true }, // TAP/BALLET/TUMBLING
+  "1088204": { price: 150, allowMultiDiscount: true }, // TAP/BALLEY/TUMBLING
 };
